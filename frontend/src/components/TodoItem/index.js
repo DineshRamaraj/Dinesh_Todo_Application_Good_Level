ea009619aad9f94543afb679465f4a492a387db2
DineshRamaraj <dineshkumarrrr01@gmail.com>
2024-10-29 21:48:39 +0530

    Updated on Frontend

--- frontend/src/components/TodoItem/index.js
+++ frontend/src/components/TodoItem/index.js
@@ -12,39 +12,43 @@ const TodoItem = ({
   //   const deleteTodo = () => {};
   return (
     <>
-      <div className="border border-slate-400 rounded-md p-4 w-[100%]">
-        <div className="flex justify-between items-center">
-          <span className="text-slate-800 text-md font-semibold text-[roboto] w-[90%]">
-            {title}
-          </span>
-          <div className="flex items-center gap-4">
-            <button
-              type="button"
-              onClick={() => {
-                setShowMainPage(true);
-                setMainContent("Update");
-                setInputHandler({ title, description, status });
-                localStorage.setItem("todoId", _id);
-              }}
-            >
-              <MdEdit size={18} color="blue" title="Edit" />
-            </button>
-            <button
-              type="button"
-              onClick={() => {
-                setShowMainPage(true);
-                setMainContent("Delete");
-                localStorage.setItem("todoId", _id);
-              }}
-            >
-              <MdDelete size={18} color="red" title="Delete" />
-            </button>
+      <div className="border border-slate-400 rounded-md p-4 w-[100%] flex flex-col justify-between">
+        <div className="flex flex-col">
+          <div className="flex justify-between items-center">
+            <span className="text-slate-800 text-md font-semibold text-[roboto] w-[90%]">
+              {title.slice(0, 12)}
+              {title.length > 12 && "..."}
+            </span>
+            <div className="flex items-center gap-4">
+              <button
+                type="button"
+                onClick={() => {
+                  setShowMainPage(true);
+                  setMainContent("Update");
+                  setInputHandler({ title, description, status });
+                  localStorage.setItem("todoId", _id);
+                }}
+              >
+                <MdEdit size={18} color="blue" title="Edit" />
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  setShowMainPage(true);
+                  setMainContent("Delete");
+                  localStorage.setItem("todoId", _id);
+                }}
+              >
+                <MdDelete size={18} color="red" title="Delete" />
+              </button>
+            </div>
           </div>
-        </div>
-        <div>
-          <span className="text-sm text-slate-400 font-thin">
-            {description}
-          </span>
+          <div>
+            <span className="text-sm text-slate-400 font-thin">
+              {description.slice(0, 50)}
+              {description.length > 50 && "..."}
+            </span>
+          </div>
         </div>
         <div className="flex items-center">
           <span
@@ -56,7 +60,9 @@ const TodoItem = ({
                 : "rounded-full bg-green-600"
             } p-1 mr-1`}
           ></span>
-          <span className="text-[12px] text-slate-400 font-light">{status}</span>
+          <span className="text-[12px] text-slate-400 font-light">
+            {status}
+          </span>
         </div>
       </div>
     </>
