3bd297931ae25b6c844e4781d5db3a989fd172ba
DineshRamaraj <dineshkumarrrr01@gmail.com>
2024-10-29 21:55:51 +0530

    Updated Ones

--- backend/src/controllers/Todo/AddTodo.js
+++ backend/src/controllers/Todo/AddTodo.js
@@ -4,6 +4,7 @@ const AddTodo = async (req, res) => {
   const { user_id } = req.params;
   console.log(req.body);
   const { title, description="This is My Description", status="Pending" } = req.body;
+  console.log(description);
   try {
     if (title.length < 3 || title === "") {
       return res.status(404).json({ message: "Invalid Title" });
